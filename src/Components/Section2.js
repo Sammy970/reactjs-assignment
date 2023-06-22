c0fc61f9a443ecc37f57aaae83e9da878a48b918
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-06-22 11:51:09 +0530

    Added fonts and sizing to make it more similar

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -3,39 +3,51 @@ import "./CSS/Section2.css";
 
 const Section2 = () => {
   return (
-    <div className="container grid grid-rows-4 gap-3 grid-flow-col justify-content-center">
+    <div className="container grid grid-rows-auto gap-6">
       <div className="heading-text">
         <h1>Sign In</h1>
         <p>Sign in to your account</p>
       </div>
 
       <div className="grid grid-flow-col auto-col-max gap-3">
-        <button className="rounded-full">Sign Up with Google</button>
-        <button className="rounded-full">Sign Up with Apple</button>
+        <button className="flex items-center justify-center gap-2">
+          <img
+            src="https://freepngimg.com/save/66274-school-google-pearl-button-up-sign-middle/1600x1600"
+            alt="google logo"
+            width="20px"
+            height="20px"
+          />
+          Sign Up with Google
+        </button>
+        <button className="flex items-center justify-center gap-2">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Apple_logo_grey.svg/1200px-Apple_logo_grey.svg.png"
+            alt="google logo"
+            width="16px"
+            height="16px"
+          />
+          Sign Up with Apple
+        </button>
       </div>
 
       <div className="login-container">
-        <div className="details">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Email Address
-          </label>
-          <div className="relative mt-2 rounded-md shadow-sm">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="0.00"
-            />
+        <div className="details grid grid-rows-auto gap-4">
+          <div>
+            <label htmlFor="email" className="block">
+              Email Address
+            </label>
+            <div className="relative mt-2 rounded-md shadow-sm">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-8 inputBox"
+                placeholder="Enter your Email"
+              />
+            </div>
           </div>
           <div>
-            <label
-              htmlFor="pass"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
+            <label htmlFor="pass" className="block">
               Password
             </label>
             <div className="relative mt-2 rounded-md shadow-sm">
@@ -43,10 +55,18 @@ const Section2 = () => {
                 type="password"
                 name="pass"
                 id="pass"
-                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="0.00"
+                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-8 text-gray-900 inputBox2"
+                placeholder="Enter your Password"
               />
             </div>
+          </div>
+
+          <div>
+            <p>Forgot password?</p>
+          </div>
+
+          <div>
+            <button className="sign-in-btn">Sign In</button>
           </div>
         </div>
       </div>
